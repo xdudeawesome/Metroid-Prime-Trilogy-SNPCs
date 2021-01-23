d08ab62f987e0737c7a720f66c4ec35e55637836
xdudeawesome <47611998+xdudeawesome@users.noreply.github.com>
2021-01-22 22:56:11 -0600

    Add files via upload

---
+++ lua/autorun/mp1_autorun.lua
@@ -0,0 +1,221 @@
+/*--------------------------------------------------
+	=============== Autorun File ===============
+	*** Copyright (c) 2012-2019 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+--------------------------------------------------*/
+------------------ Addon Information ------------------
+local PublicAddonName = "Metroid Prime SNPCs"
+local AddonName = "Metroid Prime SNPCs"
+local AddonType = "SNPC"
+local AutorunFile = "autorun/mp1_autorun.lua"
+-------------------------------------------------------
+local VJExists = file.Exists("lua/autorun/vj_base_autorun.lua","GAME")
+if VJExists == true then
+	include('autorun/vj_controls.lua')
+	local vCat = "Metroid Prime"
+	
+	VJ.AddNPC("Parasite","npc_vj_mp1_parasite",vCat)
+
+	VJ.AddNPC("Injured Pirate","npc_vj_mp1_pirate_injured",vCat)
+
+	VJ.AddNPC("Auto Defense Turret","npc_vj_mp1_autoturret",vCat)
+
+	VJ.AddNPC("Injured Pirate","npc_vj_mp1_pirate_injured2",vCat)
+
+	VJ.AddNPC("Sova","npc_vj_mp1_sova",vCat)
+
+	VJ.AddNPC("Beetle","npc_vj_mp1_beetle",vCat)
+
+	VJ.AddNPC("Sap Sac","npc_vj_mp1_sap_sac",vCat)
+
+	VJ.AddNPC("Blastcap","npc_vj_mp1_blastcap",vCat)
+
+	VJ.AddNPC("Zoomer","npc_vj_mp1_zoomer",vCat)
+
+	VJ.AddNPC("Geemer","npc_vj_mp1_geemer",vCat)
+
+	VJ.AddNPC("Scarab","npc_vj_mp1_scarab",vCat)
+
+	VJ.AddNPC("Eyon","npc_vj_mp1_eyon",vCat)
+
+	VJ.AddNPC("War Wasp","npc_vj_mp1_warwasp",vCat)
+
+	VJ.AddNPC("Tangle Weed","npc_vj_mp1_snakeweed",vCat)
+
+	VJ.AddNPC("Plazmite","npc_vj_mp1_fireflea",vCat)
+
+	VJ.AddNPC("Hive Mecha","npc_vj_mp1_hive_mecha",vCat)
+
+	VJ.AddNPC("Ram War Wasp","npc_vj_mp1_warwasp_ram",vCat)
+
+	VJ.AddNPC("Plated Beetle","npc_vj_mp1_beetle_plated",vCat)
+
+	VJ.AddNPC("Shriekbat","npc_vj_mp1_shriekbat",vCat)
+
+	VJ.AddNPC("Reaper Vine","npc_vj_mp1_reapervine",vCat)
+
+	VJ.AddNPC("Incinerator Drone","npc_vj_mp1_incinerator",vCat)
+
+	VJ.AddNPC("Barbed War Wasp","npc_vj_mp1_warwasp_barbed",vCat)
+
+	VJ.AddNPC("Stone Toad","npc_vj_mp1_puddletoad",vCat)
+
+	VJ.AddNPC("Pulse Bombu","npc_vj_mp1_atomic_alpha",vCat)
+
+	VJ.AddNPC("Oculus","npc_vj_mp1_oculus",vCat)
+
+	VJ.AddNPC("Grizby","npc_vj_mp1_grizby",vCat)
+
+	VJ.AddNPC("Burrower","npc_vj_mp1_burrower",vCat)
+
+	VJ.AddNPC("Puffer","npc_vj_mp1_puffer",vCat)
+
+	VJ.AddNPC("Magmoor","npc_vj_mp1_magmoor",vCat)
+
+	VJ.AddNPC("Triclops","npc_vj_mp1_triclops",vCat)
+
+	VJ.AddNPC("Bloodflower","npc_vj_mp1_bloodflower",vCat)
+
+	VJ.AddNPC("Seedling","npc_vj_mp1_seedling",vCat)
+
+	VJ.AddNPC("Flickerbat","npc_vj_mp1_flickerbat",vCat)
+
+	VJ.AddNPC("Crystallite","npc_vj_mp1_crystallite",vCat)
+
+	VJ.AddNPC("Scatter Bombu","npc_vj_mp1_atomic_beta",vCat)
+
+	VJ.AddNPC("Baby Sheegoth","npc_vj_mp1_babysheegoth",vCat)
+
+	VJ.AddNPC("Ice Burrower","npc_vj_mp1_burrower_ice",vCat)
+
+	VJ.AddNPC("Ice Parasite","npc_vj_mp1_parasite_ice",vCat)
+
+	VJ.AddNPC("Sheegoth","npc_vj_mp1_sheegoth",vCat)
+
+	VJ.AddNPC("Ice Shriekbat","npc_vj_mp1_shriekbat_ice",vCat)
+
+	VJ.AddNPC("Shadow Pirate","npc_vj_mp1_pirate_shadow",vCat)
+
+	VJ.AddNPC("Space Pirate","npc_vj_mp1_pirate",vCat)
+
+	VJ.AddNPC("Flying Pirate","npc_vj_mp1_pirate_flying",vCat)
+
+	VJ.AddNPC("Metroid","npc_vj_mp1_metroid",vCat)
+
+	VJ.AddNPC("Ice Beetle","npc_vj_mp1_beetle_ice",vCat)
+
+	VJ.AddNPC("Sentry Drone","npc_vj_mp1_sentrydrone",vCat)
+
+	VJ.AddNPC("Plated Parasite","npc_vj_mp1_parasite_plated",vCat)
+
+	VJ.AddNPC("Plated Puffer","npc_vj_mp1_puffer_plated",vCat)
+
+	VJ.AddNPC("Chozo Ghost","npc_vj_mp1_chozoghost",vCat)
+
+	VJ.AddNPC("Hunter Metroid","npc_vj_mp1_metroid_hunter",vCat)
+
+	VJ.AddNPC("Glider","npc_vj_mp1_glider",vCat)
+
+	VJ.AddNPC("Ripper","npc_vj_mp1_ripper",vCat)
+
+	VJ.AddNPC("Jelzap","npc_vj_mp1_jelzap",vCat)
+
+	VJ.AddNPC("Puddle Spore","npc_vj_mp1_puddlespore",vCat)
+
+	VJ.AddNPC("Tallon Crab","npc_vj_mp1_talloncrab",vCat)
+
+	VJ.AddNPC("Aqua Reaper","npc_vj_mp1_aquareaper",vCat)
+
+	VJ.AddNPC("Aqua Pirate","npc_vj_mp1_pirate_aqua",vCat)
+
+	VJ.AddNPC("Aqua Drone","npc_vj_mp1_aquadrone",vCat)
+
+	VJ.AddNPC("Aqua Sac","npc_vj_mp1_aqua_sac",vCat)
+
+	VJ.AddNPC("Mega Turret","npc_vj_mp1_autoturret_mega",vCat)
+
+	VJ.AddNPC("Wave Trooper","npc_vj_mp1_pirate_wave",vCat)
+
+	VJ.AddNPC("Power Trooper","npc_vj_mp1_pirate_power",vCat)
+
+	VJ.AddNPC("Elite Pirate","npc_vj_mp1_pirate_elite",vCat)
+
+	VJ.AddNPC("Ice Trooper","npc_vj_mp1_pirate_ice",vCat)
+
+	VJ.AddNPC("Cloaked Drone","npc_vj_mp1_cloakeddrone",vCat)
+
+	VJ.AddNPC("Phazon Elite","npc_vj_mp1_pirate_elite_ph",vCat)
+
+	VJ.AddNPC("Phazon Blastcap","npc_vj_mp1_blastcap_phazon",vCat)
+
+	VJ.AddNPC("Invisible Pulse Bombu","npc_vj_mp1_bombu_pulse_inv",vCat)
+
+	VJ.AddNPC("Plasma Trooper","npc_vj_mp1_pirate_plasma",vCat)
+
+	VJ.AddNPC("Fission Metroid","npc_vj_mp1_metroid_fission",vCat)
+
+	VJ.AddNPC("Lumigek","npc_vj_mp1_lumigek",vCat)
+
+	VJ.AddNPC("Phazon Burrower","npc_vj_mp1_burrower_phazon",vCat)
+
+	VJ.AddNPC("Parasite Queen","npc_vj_mp1_parasite_queen",vCat)
+
+	VJ.AddNPC("Flaahgra","npc_vj_mp1_flaahgra",vCat)
+
+	VJ.AddNPC("Thardus","npc_vj_mp1_thardus",vCat)
+
+	VJ.AddNPC("Omega Pirate","npc_vj_mp1_pirate_omega",vCat)
+
+	VJ.AddNPC("Meta Ridley","npc_vj_mp1_metaridley",vCat)
+
+	VJ.AddNPC("Metroid Prime","npc_vj_mp1_metroid_prime",vCat)
+
+	VJ.AddNPC("Metroid Prime (Core)","npc_vj_mp1_metroid_prime_core",vCat)
+
+	-- Particles --
+	VJ.AddParticle("particles/example_particle.pcf",{
+		"example_particle_name1",
+		"example_particle_name2",
+	})
+	
+	-- Precache Models --
+	util.PrecacheModel("models/example_model.mdl")
+	
+	-- ConVars --
+	VJ.AddConVar("vj_mp1_parasite_h",5) -- Example 1
+	VJ.AddConVar("vj_mp1_parasite_d",2) -- Example 2
+	
+	
+-- !!!!!! DON'T TOUCH ANYTHING BELOW THIS !!!!!! -------------------------------------------------------------------------------------------------------------------------
+	AddCSLuaFile(AutorunFile)
+	VJ.AddAddonProperty(AddonName,AddonType)
+else
+	if (CLIENT) then
+		chat.AddText(Color(0,200,200),PublicAddonName,
+		Color(0,255,0)," was unable to install, you are missing ",
+		Color(255,100,0),"VJ Base!")
+	end
+	timer.Simple(1,function()
+		if not VJF then
+			if (CLIENT) then
+				VJF = vgui.Create("DFrame")
+				VJF:SetTitle("ERROR!")
+				VJF:SetSize(790,560)
+				VJF:SetPos((ScrW()-VJF:GetWide())/2,(ScrH()-VJF:GetTall())/2)
+				VJF:MakePopup()
+				VJF.Paint = function()
+					draw.RoundedBox(8,0,0,VJF:GetWide(),VJF:GetTall(),Color(200,0,0,150))
+				end
+				
+				local VJURL = vgui.Create("DHTML",VJF)
+				VJURL:SetPos(VJF:GetWide()*0.005, VJF:GetTall()*0.03)
+				VJURL:Dock(FILL)
+				VJURL:SetAllowLua(true)
+				VJURL:OpenURL("https://sites.google.com/site/vrejgaming/vjbasemissing")
+			elseif (SERVER) then
+				timer.Create("VJBASEMissing",5,0,function() print("VJ Base is Missing! Download it from the workshop!") end)
+			end
+		end
+	end)
+end
---
+++ lua/autorun/mp2_autorun.lua
@@ -0,0 +1,247 @@
+/*--------------------------------------------------
+	=============== Autorun File ===============
+	*** Copyright (c) 2012-2019 by DrVrej, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+--------------------------------------------------*/
+------------------ Addon Information ------------------
+local PublicAddonName = "Metroid Prime 2: Echoes SNPCs"
+local AddonName = "Metroid Prime 2: Echoes SNPCs"
+local AddonType = "SNPC"
+local AutorunFile = "autorun/mp2_autorun.lua"
+-------------------------------------------------------
+local VJExists = file.Exists("lua/autorun/vj_base_autorun.lua","GAME")
+if VJExists == true then
+	include('autorun/vj_controls.lua')
+	local vCat = "Metroid Prime 2: Echoes"
+	
+	VJ.AddNPC("Worker Splinter","npc_vj_mp2_splinter_worker",vCat)
+
+	VJ.AddNPC("Dark Trooper","npc_vj_mp2_darktrooper",vCat)
+
+	VJ.AddNPC("Splinter","npc_vj_mp2_splinter",vCat)
+
+	VJ.AddNPC("Green Kralee","npc_vj_mp2_kralee_green",vCat)
+
+	VJ.AddNPC("War Wasp","npc_vj_mp2_warwasp",vCat)
+
+	VJ.AddNPC("GM-42 'Growler' Class Turret","npc_vj_mp2_turret_galactic",vCat)
+
+	VJ.AddNPC("Dark Splinter","npc_vj_mp2_splinter_dark",vCat)
+
+	VJ.AddNPC("Lightflyer","npc_vj_mp2_lightflyer",vCat)
+
+	VJ.AddNPC("Alpha Splinter","npc_vj_mp2_splinter_alpha",vCat)
+
+	VJ.AddNPC("Dark Alpha Splinter","npc_vj_mp2_splinter_alpha_dark",vCat)
+
+	VJ.AddNPC("Lumite","npc_vj_mp2_lumite",vCat)
+
+	VJ.AddNPC("Sandbats","npc_vj_mp2_sandbats",vCat)
+
+	VJ.AddNPC("Sandigger","npc_vj_mp2_sandworm",vCat)
+
+	VJ.AddNPC("Pirate Trooper","npc_vj_mp2_pirate",vCat)
+
+	VJ.AddNPC("Brizgee","npc_vj_mp2_brizgee",vCat)
+
+	VJ.AddNPC("Shriekbat","npc_vj_mp2_shriekbat",vCat)
+
+	VJ.AddNPC("Alpha Sandigger","npc_vj_mp2_sandworm_alpha",vCat)
+
+	VJ.AddNPC("Bomb Guardian","npc_vj_mp2_sandworm_ing",vCat)
+
+	VJ.AddNPC("Dark Pirate Trooper","npc_vj_mp2_pirate_dark",vCat)
+
+	VJ.AddNPC("Pillbug","npc_vj_mp2_pillbug",vCat)
+
+	VJ.AddNPC("Inglet","npc_vj_mp2_minoring",vCat)
+
+	VJ.AddNPC("Nightbarb","npc_vj_mp2_nightbarb",vCat)
+
+	VJ.AddNPC("Warrior Ing","npc_vj_mp2_ing",vCat)
+
+	VJ.AddNPC("Lightbringer","npc_vj_mp2_lightbringer",vCat)
+
+	VJ.AddNPC("Jump Guardian","npc_vj_mp2_ing_jump",vCat)
+
+	VJ.AddNPC("'Vigilance' Class Turret","npc_vj_mp2_turret_manned",vCat)
+
+	VJ.AddNPC("'Humility' Class Turret","npc_vj_mp2_turret",vCat)
+
+	VJ.AddNPC("Tallon Metroid","npc_vj_mp2_metroid",vCat)
+
+	VJ.AddNPC("Pirate Aerotrooper","npc_vj_mp2_pirate_aero",vCat)
+
+	VJ.AddNPC("Pirate Grenadier","npc_vj_mp2_pirate_gre",vCat)
+
+	VJ.AddNPC("Infant Tallon Metroid","npc_vj_mp2_metroid_infant",vCat)
+
+	VJ.AddNPC("Dark Preed","npc_vj_mp2_puffer_dark",vCat)
+
+	VJ.AddNPC("Darkling Tentacle","npc_vj_mp2_wisptentacle",vCat)
+
+	VJ.AddNPC("Amorbis","npc_vj_mp2_amorbis",vCat)
+
+	VJ.AddNPC("Dark Tallon Metroid","npc_vj_mp2_metroid_dark",vCat)
+
+	VJ.AddNPC("Harmony Class Drone","npc_vj_mp2_atomic_alpha",vCat)
+
+	VJ.AddNPC("Corrupted Sentreye","npc_vj_mp2_darkeye",vCat)
+
+	VJ.AddNPC("Shredder","npc_vj_mp2_shredder",vCat)
+
+	VJ.AddNPC("Hydlings","npc_vj_mp2_hydling",vCat)
+
+	VJ.AddNPC("Shrieker","npc_vj_mp2_shrieker",vCat)
+
+	VJ.AddNPC("Grenchler","npc_vj_mp2_grenchler",vCat)
+
+//	VJ.AddNPC("Phlogus","npc_vj_mp2_phlogus",vCat)
+
+	VJ.AddNPC("Dark Phlogus","npc_vj_mp2_phlogus_dark",vCat)
+
+	VJ.AddNPC("Dark Pirate Commando","npc_vj_mp2_pirate_com_d",vCat)
+
+	VJ.AddNPC("Sporb","npc_vj_mp2_sporb",vCat)
+
+//	VJ.AddNPC("Dark Sporb","npc_vj_mp2_sporb_dark",vCat)
+
+	VJ.AddNPC("Pirate Commando","npc_vj_mp2_pirate_com",vCat)
+
+	VJ.AddNPC("Hunter Ing","npc_vj_mp2_mp2_ing_med",vCat)
+
+	VJ.AddNPC("Boost Guardian","npc_vj_mp2_ing_boost",vCat)
+
+	VJ.AddNPC("Krocuss","npc_vj_mp2_krocuss",vCat)
+
+	VJ.AddNPC("Dark Missile Trooper","npc_vj_mp2_darktrooper_m",vCat)
+
+	VJ.AddNPC("Preed","npc_vj_mp2_puffer",vCat)
+
+	VJ.AddNPC("Blogg","npc_vj_mp2_blogg",vCat)
+
+	VJ.AddNPC("Bloggling","npc_vj_mp2_bloggling",vCat)
+
+	VJ.AddNPC("Seedburster","npc_vj_mp2_seedburster",vCat)
+
+	VJ.AddNPC("Alpha Blogg","npc_vj_mp2_blogg_alpha",vCat)
+
+	VJ.AddNPC("Dark Grenchler","npc_vj_mp2_grenchler_dark",vCat)
+
+	VJ.AddNPC("Dark Venom Weed","npc_vj_mp2_snakeweed_dark",vCat)
+
+	VJ.AddNPC("Grapple Guardian","npc_vj_mp2_grenchler_ing",vCat)
+
+	VJ.AddNPC("Dark Blogg","npc_vj_mp2_blogg_dark",vCat)
+
+	VJ.AddNPC("Kralee","npc_vj_mp2_kralee",vCat)
+
+	VJ.AddNPC("Airthorn","npc_vj_mp2_airthorn",vCat)
+
+	VJ.AddNPC("Chykka Larva","npc_vj_mp2_chykka_larva",vCat)
+
+	VJ.AddNPC("Dark Shredder","npc_vj_mp2_shredder_dark",vCat)
+
+	VJ.AddNPC("Chykka","npc_vj_mp2_chykka",vCat)
+
+	VJ.AddNPC("Dark Chykka","npc_vj_mp2_chykka_dark",vCat)
+
+	VJ.AddNPC("Chyklings","npc_vj_mp2_chykling",vCat)
+
+	VJ.AddNPC("Dark War Wasp","npc_vj_mp2_warwasp_dark",vCat)
+
+	VJ.AddNPC("Luminoth Turret","npc_vj_mp2_turret_luminoth",vCat)
+
+	VJ.AddNPC("'Serenity' Class Drone","npc_vj_mp2_scrubber",vCat)
+
+	VJ.AddNPC("Octopede","npc_vj_mp2_octopede",vCat)
+
+	VJ.AddNPC("Rezbit","npc_vj_mp2_rezbit",vCat)
+
+	VJ.AddNPC("Quad CM","npc_vj_mp2_quad_cm",vCat)
+
+	VJ.AddNPC("Quad MB","npc_vj_mp2_quad_mb",vCat)
+
+	VJ.AddNPC("'Diligence' Class Drone","npc_vj_mp2_atomic_beta",vCat)
+
+	VJ.AddNPC("Dark 'Diligence' Drone","npc_vj_mp2_atomic_dark",vCat)
+
+	VJ.AddNPC("Mechlops","npc_vj_mp2_mechlops",vCat)
+
+	VJ.AddNPC("Dark Quad CM","npc_vj_mp2_quad_cm_dark",vCat)
+
+	VJ.AddNPC("Dark Quad MB","npc_vj_mp2_quad_mb_dark",vCat)
+
+//	VJ.AddNPC("Kenobite","npc_vj_mp2_kenobite",vCat)
+
+	VJ.AddNPC("Mekenobite","npc_vj_mp2_mekenobite",vCat)
+
+	VJ.AddNPC("Spider Guardian","npc_vj_mp2_pillbug_ing",vCat)
+
+	VJ.AddNPC("Power Bomb Guardian","npc_vj_mp2_sporb_ing",vCat)
+
+	VJ.AddNPC("Ingsmasher","npc_vj_mp2_ingsmasher",vCat)
+
+	VJ.AddNPC("Dark Ingsmasher","npc_vj_mp2_ingsmasher_dark",vCat)
+
+	VJ.AddNPC("Quadraxis","npc_vj_mp2_quadraxis",vCat)
+
+	VJ.AddNPC("Damaged Quadraxis","npc_vj_mp2_quadraxis_hurt",vCat)
+
+	VJ.AddNPC("Shielded Head Module","npc_vj_mp2_quadraxis_head",vCat)
+
+	VJ.AddNPC("Final Head Module","npc_vj_mp2_quadraxis_head2",vCat)
+
+	VJ.AddNPC("Watchdrone","npc_vj_mp2_watchdrone",vCat)
+
+	VJ.AddNPC("Emperor Ing","npc_vj_mp2_ing_emperor",vCat)
+
+	VJ.AddNPC("Mutated Emperor Ing","npc_vj_mp2_ing_mutant",vCat)
+
+	-- Particles --
+	VJ.AddParticle("particles/example_particle.pcf",{
+		"example_particle_name1",
+		"example_particle_name2",
+	})
+	
+	-- Precache Models --
+	util.PrecacheModel("models/example_model.mdl")
+	
+	-- ConVars --
+	VJ.AddConVar("vj_mp1_parasite_h",5) -- Example 1
+	VJ.AddConVar("vj_mp1_parasite_d",2) -- Example 2
+	
+	
+-- !!!!!! DON'T TOUCH ANYTHING BELOW THIS !!!!!! -------------------------------------------------------------------------------------------------------------------------
+	AddCSLuaFile(AutorunFile)
+	VJ.AddAddonProperty(AddonName,AddonType)
+else
+	if (CLIENT) then
+		chat.AddText(Color(0,200,200),PublicAddonName,
+		Color(0,255,0)," was unable to install, you are missing ",
+		Color(255,100,0),"VJ Base!")
+	end
+	timer.Simple(1,function()
+		if not VJF then
+			if (CLIENT) then
+				VJF = vgui.Create("DFrame")
+				VJF:SetTitle("ERROR!")
+				VJF:SetSize(790,560)
+				VJF:SetPos((ScrW()-VJF:GetWide())/2,(ScrH()-VJF:GetTall())/2)
+				VJF:MakePopup()
+				VJF.Paint = function()
+					draw.RoundedBox(8,0,0,VJF:GetWide(),VJF:GetTall(),Color(200,0,0,150))
+				end
+				
+				local VJURL = vgui.Create("DHTML",VJF)
+				VJURL:SetPos(VJF:GetWide()*0.005, VJF:GetTall()*0.03)
+				VJURL:Dock(FILL)
+				VJURL:SetAllowLua(true)
+				VJURL:OpenURL("https://sites.google.com/site/vrejgaming/vjbasemissing")
+			elseif (SERVER) then
+				timer.Create("VJBASEMissing",5,0,function() print("VJ Base is Missing! Download it from the workshop!") end)
+			end
+		end
+	end)
+end
