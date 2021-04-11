18791d1531f86347ca45b0b3a4c3bd48c5c279db
X dude Awesome <47611998+xdudeawesome@users.noreply.github.com>
2021-04-10 21:13:25 -0500

    Add files via upload

--- lua/entities/npc_vj_mp1_pirate_shadow/shared.lua
+++ lua/entities/npc_vj_mp1_pirate_shadow/shared.lua
@@ -3,13 +3,13 @@ ENT.Type 			= "ai"
 ENT.PrintName 		= "Space Pirate"
 ENT.Author 			= "X dude Awesome/Lord Megamix"
 ENT.Contact 		= "DON'T!!"
-ENT.Purpose 		= "The Scourge of the Universe"
+ENT.Purpose 		= "These guys use cloaking technology!"
 ENT.Instructions 	= "Spawn it and fight it!"
 ENT.Category		= "Metroid Prime"
 
 if (CLIENT) then
-	local Name = "Space Pirate"
-	local LangName = "npc_vj_mp1_pirate"
+	local Name = "Shadow Pirate"
+	local LangName = "npc_vj_mp1_pirate_shadow"
 	language.Add(LangName, Name)
 	killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
 	language.Add("#"..LangName, Name)
