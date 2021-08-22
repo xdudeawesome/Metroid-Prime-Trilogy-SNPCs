cfddbfbfc036f40c607ea137c3bc085be9f4d5de
X dude Awesome <47611998+xdudeawesome@users.noreply.github.com>
2021-08-22 14:22:41 -0500

    Add files via upload

--- lua/autorun/mp1_autorun.lua
+++ lua/autorun/mp1_autorun.lua
@@ -17,161 +17,161 @@ if VJExists == true then
 	
 	VJ.AddNPC("Parasite","npc_vj_mp1_parasite",vCat)
 
-	VJ.AddNPC("Injured Pirate","npc_vj_mp1_pirate_injured",vCat)
+//	VJ.AddNPC("Injured Pirate","npc_vj_mp1_pirate_injured",vCat)
 
-	VJ.AddNPC("Auto Defense Turret","npc_vj_mp1_autoturret",vCat)
+//	VJ.AddNPC("Auto Defense Turret","npc_vj_mp1_autoturret",vCat)
 
-	VJ.AddNPC("Injured Pirate","npc_vj_mp1_pirate_injured2",vCat)
+//	VJ.AddNPC("Injured Pirate","npc_vj_mp1_pirate_injured2",vCat)
 
-	VJ.AddNPC("Sova","npc_vj_mp1_sova",vCat)
+//	VJ.AddNPC("Sova","npc_vj_mp1_sova",vCat)
 
 	VJ.AddNPC("Beetle","npc_vj_mp1_beetle",vCat)
 
-	VJ.AddNPC("Sap Sac","npc_vj_mp1_sap_sac",vCat)
+//	VJ.AddNPC("Sap Sac","npc_vj_mp1_sap_sac",vCat)
 
-	VJ.AddNPC("Blastcap","npc_vj_mp1_blastcap",vCat)
+//	VJ.AddNPC("Blastcap","npc_vj_mp1_blastcap",vCat)
 
-	VJ.AddNPC("Zoomer","npc_vj_mp1_zoomer",vCat)
+//	VJ.AddNPC("Zoomer","npc_vj_mp1_zoomer",vCat)
 
-	VJ.AddNPC("Geemer","npc_vj_mp1_geemer",vCat)
+//	VJ.AddNPC("Geemer","npc_vj_mp1_geemer",vCat)
 
-	VJ.AddNPC("Scarab","npc_vj_mp1_scarab",vCat)
+//	VJ.AddNPC("Scarab","npc_vj_mp1_scarab",vCat)
 
-	VJ.AddNPC("Eyon","npc_vj_mp1_eyon",vCat)
+//	VJ.AddNPC("Eyon","npc_vj_mp1_eyon",vCat)
 
-	VJ.AddNPC("War Wasp","npc_vj_mp1_warwasp",vCat)
+//	VJ.AddNPC("War Wasp","npc_vj_mp1_warwasp",vCat)
 
-	VJ.AddNPC("Tangle Weed","npc_vj_mp1_snakeweed",vCat)
+//	VJ.AddNPC("Tangle Weed","npc_vj_mp1_snakeweed",vCat)
 
-	VJ.AddNPC("Plazmite","npc_vj_mp1_fireflea",vCat)
+//	VJ.AddNPC("Plazmite","npc_vj_mp1_fireflea",vCat)
 
-	VJ.AddNPC("Hive Mecha","npc_vj_mp1_hive_mecha",vCat)
+//	VJ.AddNPC("Hive Mecha","npc_vj_mp1_hive_mecha",vCat)
 
-	VJ.AddNPC("Ram War Wasp","npc_vj_mp1_warwasp_ram",vCat)
+//	VJ.AddNPC("Ram War Wasp","npc_vj_mp1_warwasp_ram",vCat)
 
-	VJ.AddNPC("Plated Beetle","npc_vj_mp1_beetle_plated",vCat)
+//	VJ.AddNPC("Plated Beetle","npc_vj_mp1_beetle_plated",vCat)
 
-	VJ.AddNPC("Shriekbat","npc_vj_mp1_shriekbat",vCat)
+//	VJ.AddNPC("Shriekbat","npc_vj_mp1_shriekbat",vCat)
 
-	VJ.AddNPC("Reaper Vine","npc_vj_mp1_reapervine",vCat)
+//	VJ.AddNPC("Reaper Vine","npc_vj_mp1_reapervine",vCat)
 
-	VJ.AddNPC("Incinerator Drone","npc_vj_mp1_incinerator",vCat)
+//	VJ.AddNPC("Incinerator Drone","npc_vj_mp1_incinerator",vCat)
 
-	VJ.AddNPC("Barbed War Wasp","npc_vj_mp1_warwasp_barbed",vCat)
+//	VJ.AddNPC("Barbed War Wasp","npc_vj_mp1_warwasp_barbed",vCat)
 
-	VJ.AddNPC("Stone Toad","npc_vj_mp1_puddletoad",vCat)
+//	VJ.AddNPC("Stone Toad","npc_vj_mp1_puddletoad",vCat)
 
-	VJ.AddNPC("Pulse Bombu","npc_vj_mp1_atomic_alpha",vCat)
+//	VJ.AddNPC("Pulse Bombu","npc_vj_mp1_atomic_alpha",vCat)
 
-	VJ.AddNPC("Oculus","npc_vj_mp1_oculus",vCat)
+//	VJ.AddNPC("Oculus","npc_vj_mp1_oculus",vCat)
 
-	VJ.AddNPC("Grizby","npc_vj_mp1_grizby",vCat)
+//	VJ.AddNPC("Grizby","npc_vj_mp1_grizby",vCat)
 
 	VJ.AddNPC("Burrower","npc_vj_mp1_burrower",vCat)
 
-	VJ.AddNPC("Puffer","npc_vj_mp1_puffer",vCat)
+//	VJ.AddNPC("Puffer","npc_vj_mp1_puffer",vCat)
 
-	VJ.AddNPC("Magmoor","npc_vj_mp1_magmoor",vCat)
+//	VJ.AddNPC("Magmoor","npc_vj_mp1_magmoor",vCat)
 
-	VJ.AddNPC("Triclops","npc_vj_mp1_triclops",vCat)
+//	VJ.AddNPC("Triclops","npc_vj_mp1_triclops",vCat)
 
-	VJ.AddNPC("Bloodflower","npc_vj_mp1_bloodflower",vCat)
+//	VJ.AddNPC("Bloodflower","npc_vj_mp1_bloodflower",vCat)
 
-	VJ.AddNPC("Seedling","npc_vj_mp1_seedling",vCat)
+//	VJ.AddNPC("Seedling","npc_vj_mp1_seedling",vCat)
 
-	VJ.AddNPC("Flickerbat","npc_vj_mp1_flickerbat",vCat)
+//	VJ.AddNPC("Flickerbat","npc_vj_mp1_flickerbat",vCat)
 
-	VJ.AddNPC("Crystallite","npc_vj_mp1_crystallite",vCat)
+//	VJ.AddNPC("Crystallite","npc_vj_mp1_crystallite",vCat)
 
-	VJ.AddNPC("Scatter Bombu","npc_vj_mp1_atomic_beta",vCat)
+//	VJ.AddNPC("Scatter Bombu","npc_vj_mp1_atomic_beta",vCat)
 
-	VJ.AddNPC("Baby Sheegoth","npc_vj_mp1_babysheegoth",vCat)
+//	VJ.AddNPC("Baby Sheegoth","npc_vj_mp1_babysheegoth",vCat)
 
-	VJ.AddNPC("Ice Burrower","npc_vj_mp1_burrower_ice",vCat)
+//	VJ.AddNPC("Ice Burrower","npc_vj_mp1_burrower_ice",vCat)
 
-	VJ.AddNPC("Ice Parasite","npc_vj_mp1_parasite_ice",vCat)
+//	VJ.AddNPC("Ice Parasite","npc_vj_mp1_parasite_ice",vCat)
 
-	VJ.AddNPC("Sheegoth","npc_vj_mp1_sheegoth",vCat)
+//	VJ.AddNPC("Sheegoth","npc_vj_mp1_sheegoth",vCat)
 
-	VJ.AddNPC("Ice Shriekbat","npc_vj_mp1_shriekbat_ice",vCat)
+//	VJ.AddNPC("Ice Shriekbat","npc_vj_mp1_shriekbat_ice",vCat)
 
-	VJ.AddNPC("Shadow Pirate","npc_vj_mp1_pirate_shadow",vCat)
+//	VJ.AddNPC("Shadow Pirate","npc_vj_mp1_pirate_shadow",vCat)
 
-	VJ.AddNPC("Space Pirate","npc_vj_mp1_pirate",vCat)
+//	VJ.AddNPC("Space Pirate","npc_vj_mp1_pirate",vCat)
 
-	VJ.AddNPC("Flying Pirate","npc_vj_mp1_pirate_flying",vCat)
+//	VJ.AddNPC("Flying Pirate","npc_vj_mp1_pirate_flying",vCat)
 
-	VJ.AddNPC("Metroid","npc_vj_mp1_metroid",vCat)
+//	VJ.AddNPC("Metroid","npc_vj_mp1_metroid",vCat)
 
-	VJ.AddNPC("Ice Beetle","npc_vj_mp1_beetle_ice",vCat)
+//	VJ.AddNPC("Ice Beetle","npc_vj_mp1_beetle_ice",vCat)
 
-	VJ.AddNPC("Sentry Drone","npc_vj_mp1_sentrydrone",vCat)
+//	VJ.AddNPC("Sentry Drone","npc_vj_mp1_sentrydrone",vCat)
 
-	VJ.AddNPC("Plated Parasite","npc_vj_mp1_parasite_plated",vCat)
+//	VJ.AddNPC("Plated Parasite","npc_vj_mp1_parasite_plated",vCat)
 
-	VJ.AddNPC("Plated Puffer","npc_vj_mp1_puffer_plated",vCat)
+//	VJ.AddNPC("Plated Puffer","npc_vj_mp1_puffer_plated",vCat)
 
-	VJ.AddNPC("Chozo Ghost","npc_vj_mp1_chozoghost",vCat)
+//	VJ.AddNPC("Chozo Ghost","npc_vj_mp1_chozoghost",vCat)
 
-	VJ.AddNPC("Hunter Metroid","npc_vj_mp1_metroid_hunter",vCat)
+//	VJ.AddNPC("Hunter Metroid","npc_vj_mp1_metroid_hunter",vCat)
 
-	VJ.AddNPC("Glider","npc_vj_mp1_glider",vCat)
+//	VJ.AddNPC("Glider","npc_vj_mp1_glider",vCat)
 
-	VJ.AddNPC("Ripper","npc_vj_mp1_ripper",vCat)
+//	VJ.AddNPC("Ripper","npc_vj_mp1_ripper",vCat)
 
-	VJ.AddNPC("Jelzap","npc_vj_mp1_jelzap",vCat)
+//	VJ.AddNPC("Jelzap","npc_vj_mp1_jelzap",vCat)
 
-	VJ.AddNPC("Puddle Spore","npc_vj_mp1_puddlespore",vCat)
+//	VJ.AddNPC("Puddle Spore","npc_vj_mp1_puddlespore",vCat)
 
-	VJ.AddNPC("Tallon Crab","npc_vj_mp1_talloncrab",vCat)
+//	VJ.AddNPC("Tallon Crab","npc_vj_mp1_talloncrab",vCat)
 
-	VJ.AddNPC("Aqua Reaper","npc_vj_mp1_aquareaper",vCat)
+//	VJ.AddNPC("Aqua Reaper","npc_vj_mp1_aquareaper",vCat)
 
-	VJ.AddNPC("Aqua Pirate","npc_vj_mp1_pirate_aqua",vCat)
+//	VJ.AddNPC("Aqua Pirate","npc_vj_mp1_pirate_aqua",vCat)
 
-	VJ.AddNPC("Aqua Drone","npc_vj_mp1_aquadrone",vCat)
+//	VJ.AddNPC("Aqua Drone","npc_vj_mp1_aquadrone",vCat)
 
-	VJ.AddNPC("Aqua Sac","npc_vj_mp1_aqua_sac",vCat)
+//	VJ.AddNPC("Aqua Sac","npc_vj_mp1_aqua_sac",vCat)
 
-	VJ.AddNPC("Mega Turret","npc_vj_mp1_autoturret_mega",vCat)
+//	VJ.AddNPC("Mega Turret","npc_vj_mp1_autoturret_mega",vCat)
 
-	VJ.AddNPC("Wave Trooper","npc_vj_mp1_pirate_wave",vCat)
+//	VJ.AddNPC("Wave Trooper","npc_vj_mp1_pirate_wave",vCat)
 
-	VJ.AddNPC("Power Trooper","npc_vj_mp1_pirate_power",vCat)
+//	VJ.AddNPC("Power Trooper","npc_vj_mp1_pirate_power",vCat)
 
-	VJ.AddNPC("Elite Pirate","npc_vj_mp1_pirate_elite",vCat)
+//	VJ.AddNPC("Elite Pirate","npc_vj_mp1_pirate_elite",vCat)
 
-	VJ.AddNPC("Ice Trooper","npc_vj_mp1_pirate_ice",vCat)
+//	VJ.AddNPC("Ice Trooper","npc_vj_mp1_pirate_ice",vCat)
 
-	VJ.AddNPC("Cloaked Drone","npc_vj_mp1_cloakeddrone",vCat)
+//	VJ.AddNPC("Cloaked Drone","npc_vj_mp1_cloakeddrone",vCat)
 
-	VJ.AddNPC("Phazon Elite","npc_vj_mp1_pirate_elite_ph",vCat)
+//	VJ.AddNPC("Phazon Elite","npc_vj_mp1_pirate_elite_ph",vCat)
 
-	VJ.AddNPC("Phazon Blastcap","npc_vj_mp1_blastcap_phazon",vCat)
+//	VJ.AddNPC("Phazon Blastcap","npc_vj_mp1_blastcap_phazon",vCat)
 
-	VJ.AddNPC("Invisible Pulse Bombu","npc_vj_mp1_bombu_pulse_inv",vCat)
+//	VJ.AddNPC("Invisible Pulse Bombu","npc_vj_mp1_bombu_pulse_inv",vCat)
 
-	VJ.AddNPC("Plasma Trooper","npc_vj_mp1_pirate_plasma",vCat)
+//	VJ.AddNPC("Plasma Trooper","npc_vj_mp1_pirate_plasma",vCat)
 
-	VJ.AddNPC("Fission Metroid","npc_vj_mp1_metroid_fission",vCat)
+//	VJ.AddNPC("Fission Metroid","npc_vj_mp1_metroid_fission",vCat)
 
-	VJ.AddNPC("Lumigek","npc_vj_mp1_lumigek",vCat)
+//	VJ.AddNPC("Lumigek","npc_vj_mp1_lumigek",vCat)
 
-	VJ.AddNPC("Phazon Burrower","npc_vj_mp1_burrower_phazon",vCat)
+//	VJ.AddNPC("Phazon Burrower","npc_vj_mp1_burrower_phazon",vCat)
 
-	VJ.AddNPC("Parasite Queen","npc_vj_mp1_parasite_queen",vCat)
+//	VJ.AddNPC("Parasite Queen","npc_vj_mp1_parasite_queen",vCat)
 
-	VJ.AddNPC("Flaahgra","npc_vj_mp1_flaahgra",vCat)
+//	VJ.AddNPC("Flaahgra","npc_vj_mp1_flaahgra",vCat)
 
-	VJ.AddNPC("Thardus","npc_vj_mp1_thardus",vCat)
+//	VJ.AddNPC("Thardus","npc_vj_mp1_thardus",vCat)
 
-	VJ.AddNPC("Omega Pirate","npc_vj_mp1_pirate_omega",vCat)
+//	VJ.AddNPC("Omega Pirate","npc_vj_mp1_pirate_omega",vCat)
 
-	VJ.AddNPC("Meta Ridley","npc_vj_mp1_metaridley",vCat)
+//	VJ.AddNPC("Meta Ridley","npc_vj_mp1_metaridley",vCat)
 
-	VJ.AddNPC("Metroid Prime","npc_vj_mp1_metroid_prime",vCat)
+//	VJ.AddNPC("Metroid Prime","npc_vj_mp1_metroid_prime",vCat)
 
-	VJ.AddNPC("Metroid Prime (Core)","npc_vj_mp1_metroid_prime_core",vCat)
+//	VJ.AddNPC("Metroid Prime (Core)","npc_vj_mp1_metroid_prime_core",vCat)
 
 	-- Particles --
 	VJ.AddParticle("particles/example_particle.pcf",{
--- lua/autorun/mp3_autorun.lua
+++ lua/autorun/mp3_autorun.lua
@@ -15,193 +15,193 @@ if VJExists == true then
 	include('autorun/vj_controls.lua')
 	local vCat = "Metroid Prime 3: Corruption"
 	
-	VJ.AddNPC("Pirate Trooper","npc_vj_mp3_pirate",vCat)
+//	VJ.AddNPC("Pirate Trooper","npc_vj_mp3_pirate",vCat)
 
-	VJ.AddNPC("Pirate Militia","npc_vj_mp3_pirate_militia",vCat)
+//	VJ.AddNPC("Pirate Militia","npc_vj_mp3_pirate_militia",vCat)
 
-	VJ.AddNPC("Crawltank","npc_vj_mp3_crawltank",vCat)
+//	VJ.AddNPC("Crawltank","npc_vj_mp3_crawltank",vCat)
 
-	VJ.AddNPC("Crawlmine","npc_vj_mp3_crawlmine",vCat)
+//	VJ.AddNPC("Crawlmine","npc_vj_mp3_crawlmine",vCat)
 
-	VJ.AddNPC("Aeromine","npc_vj_mp3_aeromine",vCat)
+//	VJ.AddNPC("Aeromine","npc_vj_mp3_aeromine",vCat)
 
-	VJ.AddNPC("Jumpmine","npc_vj_mp3_jumpmine",vCat)
+//	VJ.AddNPC("Jumpmine","npc_vj_mp3_jumpmine",vCat)
 
-	VJ.AddNPC("Aerotrooper","npc_vj_mp3_pirate_aero",vCat)
+//	VJ.AddNPC("Aerotrooper","npc_vj_mp3_pirate_aero",vCat)
 
-	VJ.AddNPC("'Jolly Roger' Drone","npc_vj_mp3_piratedrone",vCat)
+//	VJ.AddNPC("'Jolly Roger' Drone","npc_vj_mp3_piratedrone",vCat)
 
-	VJ.AddNPC("'Dragoon' Battle Drone","npc_vj_mp3_steamdrone",vCat)
+//	VJ.AddNPC("'Dragoon' Battle Drone","npc_vj_mp3_steamdrone",vCat)
 
-	VJ.AddNPC("Atomic","npc_vj_mp3_atomic",vCat)
+//	VJ.AddNPC("Atomic","npc_vj_mp3_atomic",vCat)
 
-	VJ.AddNPC("Armored Pirate Trooper","npc_vj_mp3_pirate_armored",vCat)
+//	VJ.AddNPC("Armored Pirate Trooper","npc_vj_mp3_pirate_armored",vCat)
 
-	VJ.AddNPC("Advanced Pirate Trooper","npc_vj_mp3_pirate_advanced",vCat)
+//	VJ.AddNPC("Advanced Pirate Trooper","npc_vj_mp3_pirate_advanced",vCat)
 
-	VJ.AddNPC("Shield Pirate Trooper","npc_vj_mp3_pirate_shield",vCat)
+//	VJ.AddNPC("Shield Pirate Trooper","npc_vj_mp3_pirate_shield",vCat)
 
-	VJ.AddNPC("Armored Pirate Militia","npc_vj_mp3_pirate_mil_arm",vCat)
+//	VJ.AddNPC("Armored Pirate Militia","npc_vj_mp3_pirate_mil_arm",vCat)
 
-	VJ.AddNPC("Shield Pirate Militia","npc_vj_mp3_pirate_mil_s",vCat)
+//	VJ.AddNPC("Shield Pirate Militia","npc_vj_mp3_pirate_mil_s",vCat)
 
-	VJ.AddNPC("Armored Shield Trooper","npc_vj_mp3_pirate_sh_arm",vCat)
+//	VJ.AddNPC("Armored Shield Trooper","npc_vj_mp3_pirate_sh_arm",vCat)
 
-	VJ.AddNPC("Advanced Shield Trooper","npc_vj_mp3_pirate_sh_adv",vCat)
+//	VJ.AddNPC("Advanced Shield Trooper","npc_vj_mp3_pirate_sh_adv",vCat)
 
-	VJ.AddNPC("Pirate Hussar","npc_vj_mp3_pirate_hussar",vCat)
+//	VJ.AddNPC("Pirate Hussar","npc_vj_mp3_pirate_hussar",vCat)
 
-	VJ.AddNPC("Advanced Aerotrooper","npc_vj_mp3_pirate_aero_adv",vCat)
+//	VJ.AddNPC("Advanced Aerotrooper","npc_vj_mp3_pirate_aero_adv",vCat)
 
-	VJ.AddNPC("Armored Aerotrooper","npc_vj_mp3_pirate_aero_arm",vCat)
+//	VJ.AddNPC("Armored Aerotrooper","npc_vj_mp3_pirate_aero_arm",vCat)
 
-	VJ.AddNPC("Assault Pirate Trooper","npc_vj_mp3_pirate_assault",vCat)
+//	VJ.AddNPC("Assault Pirate Trooper","npc_vj_mp3_pirate_assault",vCat)
 
-	VJ.AddNPC("Assault Shield Trooper","npc_vj_mp3_pirate_sh_assault",vCat)
+//	VJ.AddNPC("Assault Shield Trooper","npc_vj_mp3_pirate_sh_assault",vCat)
 
-	VJ.AddNPC("Assault Aerotrooper","npc_vj_mp3_pirate_aero_assault",vCat)
+//	VJ.AddNPC("Assault Aerotrooper","npc_vj_mp3_pirate_aero_assault",vCat)
 
-	VJ.AddNPC("Commando Pirate","npc_vj_mp3_pirate_commando",vCat)
+//	VJ.AddNPC("Commando Pirate","npc_vj_mp3_pirate_commando",vCat)
 
-	VJ.AddNPC("Pirate Commander","npc_vj_mp3_pirate_commander",vCat)
+//	VJ.AddNPC("Pirate Commander","npc_vj_mp3_pirate_commander",vCat)
 
-	VJ.AddNPC("Berserker Knight","npc_vj_mp3_berserker",vCat)
+//	VJ.AddNPC("Berserker Knight","npc_vj_mp3_berserker",vCat)
 
-	VJ.AddNPC("Berserker Lord","npc_vj_mp3_berserker_lord",vCat)
+//	VJ.AddNPC("Berserker Lord","npc_vj_mp3_berserker_lord",vCat)
 
-	VJ.AddNPC("Phazon Metroid","npc_vj_mp3_metroid",vCat)
+//	VJ.AddNPC("Phazon Metroid","npc_vj_mp3_metroid",vCat)
 
-	VJ.AddNPC("Miniroid","npc_vj_mp3_miniroid",vCat)
+//	VJ.AddNPC("Miniroid","npc_vj_mp3_miniroid",vCat)
 
-	VJ.AddNPC("Metroid Hatcher","npc_vj_mp3_metroid_hatcher",vCat)
+//	VJ.AddNPC("Metroid Hatcher","npc_vj_mp3_metroid_hatcher",vCat)
 
-	VJ.AddNPC("Reptilicus","npc_vj_mp3_reptilicus",vCat)
+//	VJ.AddNPC("Reptilicus","npc_vj_mp3_reptilicus",vCat)
 
-	VJ.AddNPC("Reptilicus Hunter","npc_vj_mp3_reptilicus_hunter",vCat)
+//	VJ.AddNPC("Reptilicus Hunter","npc_vj_mp3_reptilicus_hunter",vCat)
 
-	VJ.AddNPC("Warp Hound","npc_vj_mp3_blinkwolf",vCat)
+//	VJ.AddNPC("Warp Hound","npc_vj_mp3_blinkwolf",vCat)
 
-	VJ.AddNPC("Fargul Hatcher","npc_vj_mp3_fargul_hatcher",vCat)
+//	VJ.AddNPC("Fargul Hatcher","npc_vj_mp3_fargul_hatcher",vCat)
 
-	VJ.AddNPC("Fargul Wasp","npc_vj_mp3_fargul_wasp",vCat)
+//	VJ.AddNPC("Fargul Wasp","npc_vj_mp3_fargul_wasp",vCat)
 
-	VJ.AddNPC("Gragnol","npc_vj_mp3_gragnol",vCat)
+//	VJ.AddNPC("Gragnol","npc_vj_mp3_gragnol",vCat)
 
-	VJ.AddNPC("Gragnol Adult","npc_vj_mp3_gragnol_adult",vCat)
+//	VJ.AddNPC("Gragnol Adult","npc_vj_mp3_gragnol_adult",vCat)
 
-	VJ.AddNPC("Tinbot","npc_vj_mp3_tinbot",vCat)
+//	VJ.AddNPC("Tinbot","npc_vj_mp3_tinbot",vCat)
 
-	VJ.AddNPC("Steambot","npc_vj_mp3_steambot",vCat)
+//	VJ.AddNPC("Steambot","npc_vj_mp3_steambot",vCat)
 
-	VJ.AddNPC("Steamlord","npc_vj_mp3_steamlord",vCat)
+//	VJ.AddNPC("Steamlord","npc_vj_mp3_steamlord",vCat)
 
-	VJ.AddNPC("Korakk Beast","npc_vj_mp3_korakk",vCat)
+//	VJ.AddNPC("Korakk Beast","npc_vj_mp3_korakk",vCat)
 
-	VJ.AddNPC("Defense Drone","npc_vj_mp3_mechanoid",vCat)
+//	VJ.AddNPC("Defense Drone","npc_vj_mp3_mechanoid",vCat)
 
-	VJ.AddNPC("Hopper","npc_vj_mp3_hopper",vCat)
+//	VJ.AddNPC("Hopper","npc_vj_mp3_hopper",vCat)
 
-	VJ.AddNPC("Alpha Hopper","npc_vj_mp3_hopper_alpha",vCat)
+//	VJ.AddNPC("Alpha Hopper","npc_vj_mp3_hopper_alpha",vCat)
 
-	VJ.AddNPC("Metroid Hopper","npc_vj_mp3_metroid_hopper",vCat)
+//	VJ.AddNPC("Metroid Hopper","npc_vj_mp3_metroid_hopper",vCat)
 
-	VJ.AddNPC("Phazon Hopper","npc_vj_mp3_phazon_hopper",vCat)
+//	VJ.AddNPC("Phazon Hopper","npc_vj_mp3_phazon_hopper",vCat)
 
-	VJ.AddNPC("Gel Puffer","npc_vj_mp3_puffer_gel",vCat)
+//	VJ.AddNPC("Gel Puffer","npc_vj_mp3_puffer_gel",vCat)
 
-	VJ.AddNPC("Sky Puffer","npc_vj_mp3_puffer_sky",vCat)
+//	VJ.AddNPC("Sky Puffer","npc_vj_mp3_puffer_sky",vCat)
 
-	VJ.AddNPC("Puffer Mine","npc_vj_mp3_puffer_mine",vCat)
+//	VJ.AddNPC("Puffer Mine","npc_vj_mp3_puffer_mine",vCat)
 
-	VJ.AddNPC("Phazon Puffer","npc_vj_mp3_puffer_phazon",vCat)
+//	VJ.AddNPC("Phazon Puffer","npc_vj_mp3_puffer_phazon",vCat)
 
-	VJ.AddNPC("Nightbarb","npc_vj_mp3_nightbarb",vCat)
+//	VJ.AddNPC("Nightbarb","npc_vj_mp3_nightbarb",vCat)
 
-	VJ.AddNPC("Phazon Nightbarb","npc_vj_mp3_nightbarb_phazon",vCat)
+//	VJ.AddNPC("Phazon Nightbarb","npc_vj_mp3_nightbarb_phazon",vCat)
 
-	VJ.AddNPC("Swarmbot","npc_vj_mp3_swarmbot",vCat)
+//	VJ.AddNPC("Swarmbot","npc_vj_mp3_swarmbot",vCat)
 
-	VJ.AddNPC("Steamspider","npc_vj_mp3_steamspider",vCat)
+//	VJ.AddNPC("Steamspider","npc_vj_mp3_steamspider",vCat)
 
-	VJ.AddNPC("Vaporwing","npc_vj_mp3_vaporwing",vCat)
+//	VJ.AddNPC("Vaporwing","npc_vj_mp3_vaporwing",vCat)
 
-	VJ.AddNPC("Zapper","npc_vj_mp3_zapper",vCat)
+//	VJ.AddNPC("Zapper","npc_vj_mp3_zapper",vCat)
 
-	VJ.AddNPC("Shellbug","npc_vj_mp3_shellbug",vCat)
+//	VJ.AddNPC("Shellbug","npc_vj_mp3_shellbug",vCat)
 
-	VJ.AddNPC("Gelbug","npc_vj_mp3_gelbug",vCat)
+//	VJ.AddNPC("Gelbug","npc_vj_mp3_gelbug",vCat)
 
-	VJ.AddNPC("Scorchbug","npc_vj_mp3_scorchbug",vCat)
+//	VJ.AddNPC("Scorchbug","npc_vj_mp3_scorchbug",vCat)
 
-	VJ.AddNPC("Gelsac","npc_vj_mp3_jelsac",vCat)
+//	VJ.AddNPC("Gelsac","npc_vj_mp3_jelsac",vCat)
 
-	VJ.AddNPC("Gel Ray","npc_vj_mp3_gel_ray",vCat)
+//	VJ.AddNPC("Gel Ray","npc_vj_mp3_gel_ray",vCat)
 
-	VJ.AddNPC("Phaazoid","npc_vj_mp3_phazoid",vCat)
+//	VJ.AddNPC("Phaazoid","npc_vj_mp3_phazoid",vCat)
 
-	VJ.AddNPC("Small Phaazoid","npc_vj_mp3_phazoid_small",vCat)
+//	VJ.AddNPC("Small Phaazoid","npc_vj_mp3_phazoid_small",vCat)
 
-	VJ.AddNPC("Red Phaazoid","npc_vj_mp3_phazoid_red",vCat)
+//	VJ.AddNPC("Red Phaazoid","npc_vj_mp3_phazoid_red",vCat)
 
-	VJ.AddNPC("Phazon Harvester Drone","npc_vj_mp3_harvester",vCat)
+//	VJ.AddNPC("Phazon Harvester Drone","npc_vj_mp3_harvester",vCat)
 
-	VJ.AddNPC("Phazon Grub","npc_vj_mp3_phazon_grub",vCat)
+//	VJ.AddNPC("Phazon Grub","npc_vj_mp3_phazon_grub",vCat)
 
-	VJ.AddNPC("Phazon Leech","npc_vj_mp3_phazon_leech",vCat)
+//	VJ.AddNPC("Phazon Leech","npc_vj_mp3_phazon_leech",vCat)
 
-	VJ.AddNPC("Liquid Phazon","npc_vj_mp3_phazon_puddle",vCat)
+//	VJ.AddNPC("Liquid Phazon","npc_vj_mp3_phazon_puddle",vCat)
 
-	VJ.AddNPC("Phazon Pillbug","npc_vj_mp3_pillbug_phazon",vCat)
+//	VJ.AddNPC("Phazon Pillbug","npc_vj_mp3_pillbug_phazon",vCat)
 
-	VJ.AddNPC("Korba","npc_vj_mp3_korba",vCat)
+//	VJ.AddNPC("Korba","npc_vj_mp3_korba",vCat)
 
-	VJ.AddNPC("Snatcher","npc_vj_mp3_snatcher",vCat)
+//	VJ.AddNPC("Snatcher","npc_vj_mp3_snatcher",vCat)
 
-	VJ.AddNPC("Maw","npc_vj_mp3_maw",vCat)
+//	VJ.AddNPC("Maw","npc_vj_mp3_maw",vCat)
 
-	VJ.AddNPC("Aazelion","npc_vj_mp3_aazelion",vCat)
+//	VJ.AddNPC("Aazelion","npc_vj_mp3_aazelion",vCat)
 
-	VJ.AddNPC("Metroid Egg","npc_vj_mp3_metroid_egg",vCat)
+//	VJ.AddNPC("Metroid Egg","npc_vj_mp3_metroid_egg",vCat)
 
-	VJ.AddNPC("Phazon Weed","npc_vj_mp3_phazon_weed",vCat)
+//	VJ.AddNPC("Phazon Weed","npc_vj_mp3_phazon_weed",vCat)
 
-	VJ.AddNPC("Bryyonian Shriekbat","npc_vj_mp3_shriekbat_bryyo",vCat)
+//	VJ.AddNPC("Bryyonian Shriekbat","npc_vj_mp3_shriekbat_bryyo",vCat)
 
-	VJ.AddNPC("Elysian Shriekbat","npc_vj_mp3_shriekbat_elysia",vCat)
+//	VJ.AddNPC("Elysian Shriekbat","npc_vj_mp3_shriekbat_elysia",vCat)
 
-	VJ.AddNPC("Urtragian Shriekbat","npc_vj_mp3_shriekbat_pirate",vCat)
+//	VJ.AddNPC("Urtragian Shriekbat","npc_vj_mp3_shriekbat_pirate",vCat)
 
-	VJ.AddNPC("Phazon Shriekbat","npc_vj_mp3_shriekbat_phazon",vCat)
+//	VJ.AddNPC("Phazon Shriekbat","npc_vj_mp3_shriekbat_phazon",vCat)
 
-	VJ.AddNPC("'Halberd' Class Turret","npc_vj_mp3_turret_halberd",vCat)
+//	VJ.AddNPC("'Halberd' Class Turret","npc_vj_mp3_turret_halberd",vCat)
 
-	VJ.AddNPC("'Despair' Class Turret","npc_vj_mp3_turret_despair",vCat)
+//	VJ.AddNPC("'Despair' Class Turret","npc_vj_mp3_turret_despair",vCat)
 
-	VJ.AddNPC("'Remorse' Class Turret","npc_vj_mp3_turret_remorse",vCat)
+//	VJ.AddNPC("'Remorse' Class Turret","npc_vj_mp3_turret_remorse",vCat)
 
-	VJ.AddNPC("EyeballBlocker","npc_vj_mp3_eyeballblocker",vCat)
+//	VJ.AddNPC("EyeballBlocker","npc_vj_mp3_eyeballblocker",vCat)
 
-	VJ.AddNPC("Phaz-Ing","npc_vj_mp3_minoring",vCat)
+//	VJ.AddNPC("Phaz-Ing","npc_vj_mp3_minoring",vCat)
 
 //	VJ.AddNPC("Admiral Dane","npc_vj_mp3_admiral_dane",vCat)
 
-	VJ.AddNPC("Federation Marine","npc_vj_mp3_trooper",vCat)
+//	VJ.AddNPC("Federation Marine","npc_vj_mp3_trooper",vCat)
 
-	VJ.AddNPC("PED Trooper","npc_vj_mp3_trooper_PED",vCat)
+//	VJ.AddNPC("PED Trooper","npc_vj_mp3_trooper_PED",vCat)
 
-	VJ.AddNPC("Demolition Trooper","npc_vj_mp3_trooper_demo",vCat)
+//	VJ.AddNPC("Demolition Trooper","npc_vj_mp3_trooper_demo",vCat)
 
-	VJ.AddNPC("Hunter Rundas","npc_vj_mp3_rundas",vCat)
+//	VJ.AddNPC("Hunter Rundas","npc_vj_mp3_rundas",vCat)
 
-	VJ.AddNPC("Hunter Ghor","npc_vj_mp3_ghor",vCat)
+//	VJ.AddNPC("Hunter Ghor","npc_vj_mp3_ghor",vCat)
 
-	VJ.AddNPC("Hunter Gandrayda","npc_vj_mp3_gandrayda",vCat)
+//	VJ.AddNPC("Hunter Gandrayda","npc_vj_mp3_gandrayda",vCat)
 
-	VJ.AddNPC("Rundas","npc_vj_mp3_rundas_PED",vCat)
+//	VJ.AddNPC("Rundas","npc_vj_mp3_rundas_PED",vCat)
 
-	VJ.AddNPC("Ghor","npc_vj_mp3_ghor_PED",vCat)
+//	VJ.AddNPC("Ghor","npc_vj_mp3_ghor_PED",vCat)
 
-	VJ.AddNPC("Gandrayda","npc_vj_mp3_gandrayda_PED",vCat)
+//	VJ.AddNPC("Gandrayda","npc_vj_mp3_gandrayda_PED",vCat)
 
 //	VJ.AddNPC("Aerotrooper-G","npc_vj_mp3_aerotrooper-g",vCat)
 
@@ -217,15 +217,15 @@ if VJExists == true then
 
 //	VJ.AddNPC("Samus-G","npc_vj_mp3_samus-g",vCat)
 
-	VJ.AddNPC("Mogenar","npc_vj_mp3_mogenar",vCat)
+//	VJ.AddNPC("Mogenar","npc_vj_mp3_mogenar",vCat)
 
-	VJ.AddNPC("Helios","npc_vj_mp3_helios",vCat)
+//	VJ.AddNPC("Helios","npc_vj_mp3_helios",vCat)
 
-	VJ.AddNPC("Omega Ridley","npc_vj_mp3_omegaridley",vCat)
+//	VJ.AddNPC("Omega Ridley","npc_vj_mp3_omegaridley",vCat)
 
-	VJ.AddNPC("Aurora Unit 313","npc_vj_mp3_au313",vCat)
+//	VJ.AddNPC("Aurora Unit 313","npc_vj_mp3_au313",vCat)
 
-	VJ.AddNPC("Aurora Unit 313 - Head","npc_vj_mp3_au313_head",vCat)
+//	VJ.AddNPC("Aurora Unit 313 - Head","npc_vj_mp3_au313_head",vCat)
 
 	
 
